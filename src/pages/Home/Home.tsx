24bd9c9c201be50b81e00f9190c1dd55575c356c
Danyl Fernandes <androidan1304@gmail.com>
2021-11-07 02:26:40 +0530

    :sparkles: Add new components to homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Button } from "../../components/Button";
 import { Card } from "../../components/Card/Card";
 import { Container } from "../../components/Container";
+import { Input } from "../../components/Input";
 import { Navbar } from "../../components/Navbar";
 
 const Home = (): JSX.Element => {
@@ -8,7 +10,19 @@ const Home = (): JSX.Element => {
     <React.Fragment>
       <Navbar title="Let's get your document"></Navbar>
       <Container>
-        <Card>test</Card>
+        <Card>
+          <Input title="Your name" />
+          <Input
+            title="Your XIE ID"
+            info="This will be your username. Make sure you type it correctly."
+          />
+          <Input
+            title="Doc. details"
+            info="What's this doc. about? Try to keep it short."
+          />
+          <Input title="Document" />
+        </Card>
+        <Button>Yes, Upload it!</Button>
       </Container>
     </React.Fragment>
   );
