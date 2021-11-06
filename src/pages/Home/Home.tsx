902c8d86ba427867add8d984136bb86ac3decbb5
Danyl Fernandes <androidan1304@gmail.com>
2021-11-06 23:17:45 +0530

    :recycle: Move Navbar out of Container
    - The Navbar must fill the screen width at all times and sizes.
    - To accomodate this behaviour the Navbar was moved outside the Container.
    - This way the Container's content can always remain centered.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,16 @@
+import React from "react";
+import { Card } from "../../components/Card/Card";
 import { Container } from "../../components/Container";
 import { Navbar } from "../../components/Navbar";
 
 const Home = (): JSX.Element => {
   return (
-    <Container>
+    <React.Fragment>
       <Navbar title="Let's get your document"></Navbar>
-    </Container>
+      <Container>
+        <Card>test</Card>
+      </Container>
+    </React.Fragment>
   );
 };
 
