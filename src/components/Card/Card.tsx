1e65b326ecfb9083276212c2e03292d39d10f11b
Danyl Fernandes <androidan1304@gmail.com>
2021-11-07 01:38:06 +0530

    :sparkles: Add new prop 'spacing' for spacing card-items

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,13 +7,26 @@ declare type Color = keyof typeof colors;
 
 interface StyledCardProps {
   backgroundColor?: Color;
+  spacing?: number;
 }
 
 const StyledCard = styled.div<StyledCardProps>(
-  ({ backgroundColor }) => css`
+  ({ backgroundColor, spacing }) => css`
     background-color: ${backgroundColor ? colors[backgroundColor] : "white"};
     border-radius: 8px;
+    box-sizing: border-box;
     padding: 22px;
+    margin-bottom: 16px;
+
+    & > *:not(:last-child) {
+      ${spacing
+        ? css`
+            margin-bottom: ${spacing}px;
+          `
+        : css`
+            margin-bottom: 22px;
+          `}
+    }
   `
 );
 
@@ -21,14 +34,23 @@ interface CardProps {
   /** The background color of the Card */
   backgroundColor?: Color;
 
+  /** The spacing between children (in px) */
+  spacing?: number;
+
   /** The children inside the Card */
   children?: React.ReactNode | React.ReactNode[];
 }
 
-const Card = ({ backgroundColor, children }: CardProps): JSX.Element => {
+const Card = ({
+  backgroundColor,
+  spacing,
+  children,
+}: CardProps): JSX.Element => {
   return (
     <React.Fragment>
-      <StyledCard backgroundColor={backgroundColor}>{children}</StyledCard>
+      <StyledCard backgroundColor={backgroundColor} spacing={spacing}>
+        {children}
+      </StyledCard>
     </React.Fragment>
   );
 };
