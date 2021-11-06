9cfeacaa70edf8eabbfa3fe486b1f385ec6add1b
Danyl Fernandes <androidan1304@gmail.com>
2021-11-06 19:12:56 +0530

    :sparkles: Add Navbar component to homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
 import { Container } from "../../components/Container";
+import { Navbar } from "../../components/Navbar";
 
 const Home = (): JSX.Element => {
-  return <Container>hello world</Container>;
+  return (
+    <Container>
+      <Navbar title="Let's get your document"></Navbar>
+    </Container>
+  );
 };
 
 export { Home };
