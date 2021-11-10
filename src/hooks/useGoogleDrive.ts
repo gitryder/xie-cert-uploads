59602b9cdcd0d7d834b608a2a11130140031d487
Danyl Fernandes <androidan1304@gmail.com>
2021-11-10 22:54:46 +0530

    :seedling: Add new hook for Google Drive transactions

---
+++ src/hooks/useGoogleDrive.ts
@@ -0,0 +1,107 @@
+/**
+ * @author Danyl Fernandes <realllydan@gmail.com>
+ * @description A helper hook that abstracts Google Drive v3 API transactions.
+ */
+
+import { google } from "googleapis";
+
+/**
+ * @remarks
+ * Add your own client id, secret & refresh-token here.
+ * You may generate your OAuth secrets after creating a new project.
+ *
+ * @reference https://developers.google.com/workspace/guides/create-project
+ */
+const CLIENT_ID =
+  "550524347308-r3uptpg9ci1qt9b6nk15itllhnngui4u.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REFRESH_TOKEN =
+  "1//04Ke2kxnfBHHzCgYIARAAGAQSNwF-L9IrRSEAiTQo6CvIQZNV69cKBRdCjsRW1CS10eCMrpQ0kiBpR67v5ptT_Ila98ZADyotmP8";
+
+const REDIRECT_URI = "https://developer.google.com/oauthplayground";
+
+const oAuthClient = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
+oAuthClient.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const drive = google.drive({
+  version: "v3",
+  auth: oAuthClient,
+});
+
+/**
+ * !IMPORTANT: Replace the fs.createReadStream(file), with something browser-friendly.
+ *
+ * @param {*} file
+ * @param {*} fileUploadName
+ * @param {*} fileMimeType
+ * @returns
+ */
+const uploadFileToDrive = (file, fileUploadName, fileMimeType) => {
+  try {
+    const response = await drive.files.create({
+      requestBody: {
+        name: fileUploadName,
+        mimeType: fileMimeType,
+      },
+      media: {
+        mimeType: fileMimeType,
+        body: fs.createReadStream(file),
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+};
+
+const makeDriveFilePubliclyAccessible = (fileId) => {
+  try {
+    const response = await drive.permissions.create({
+      fileId: fileId,
+      requestBody: {
+        role: "reader",
+        type: "anyone",
+      },
+    });
+  } catch (err) {
+    return err;
+  }
+};
+
+const deleteFileFromDrive = (fileId) => {
+  try {
+    const response = await drive.files.delete({
+      fileId: fileId,
+    });
+  } catch (err) {
+    return err;
+  }
+};
+
+const;
+
+const getFilePublicViewUrl = (fileId) => {
+  try {
+    const response = await drive.files.get({
+      fileId: fileId,
+      fields: "webViewLink",
+    });
+  } catch (err) {
+    return err;
+  }
+};
+
+const getFilePublicDownloadUrl = (fileId) => {
+  try {
+    const response = await drive.files.get({
+      fileId: fileId,
+      fields: "webContentLink",
+    });
+  } catch (err) {
+    return err;
+  }
+};
